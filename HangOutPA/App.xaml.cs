8d8c5ba39bb63743883ecd6fa3bf99fa2b1b154c
Cholponai <cholponay112@gmail.com>
2026-05-08 10:14:30 +0200

    Add project files.

---
+++ HangOutPA/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HangOutPA
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ HangOutPA/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HangOutPA
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
